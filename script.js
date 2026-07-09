48dbe718ba4d8ff376c98a6d2b87d4dfe5d293f9
paolazuareth-dotcom <paola.zuareth@escola.pr.gov.br>
2026-07-09 15:16:05 +0000

    v6

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-///cria um constante que seliciona o botão no html
-const botao = document.querySelector("button");
+const botoes = document.querySelectorAll("button");
+botoes.forEach(function(botao) {
 botao.addEventListener("click", botaoClicado);
 
-///cria a função que fará com que o texto do numero zero some + 1 a cada click.
 function botaoClicado() {
-   let texto = botao.querySelector("span");
-   texto.textContent++;
+console.log("fui clicado");
+let texto = botao.querySelector("span");
+texto.textContent++;
 }
-
-//###legendas:
-//querySelector: seleciona o primeiro elemento que corresponde a umselector CSS especficado.
-//addEventListener: adiciona um evento de click, permintindo que voce execute uma função quando o evento ocorrer.
-//textContent: modifica o conteudo de texto de um elemento html
+});
